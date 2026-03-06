3c6407fd133eb5afb2d53e25f19187eea53513f9
Jonathan Bessette <jonathan.bessette@bell.ca>
2026-03-06 15:14:52 -0500

    release

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,14 @@
 'use client';
 
-import { Box, Button, Flex, Heading, Input, Text, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
